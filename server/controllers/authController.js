546363937bfae7bc905502c8564d915e9dd5c43a
rashmi patil <rashmipatil8849@gmail.com>
2026-02-15 20:50:01 +0530

    First commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.register = async (req, res) => {
+exports.register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
@@ -11,10 +11,12 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     const token = jwt.sign(
@@ -24,29 +26,31 @@ exports.register = async (req, res) => {
     );
 
     res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token,  
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
     });
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
-exports.login = async (req, res) => {
+
+exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
@@ -60,6 +64,7 @@ exports.login = async (req, res) => {
     res.json({ token });
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
